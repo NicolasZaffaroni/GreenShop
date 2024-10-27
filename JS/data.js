b94527eb9fb26b89502ee273af53e8ccf4f2c921
Nicolas Zaffaroni <117034184+NicolasZaffaroni@users.noreply.github.com>
2024-10-27 21:47:32 +0100

    Creado 'base de datos', corregido estetica de paginas segun lo requerido

---
+++ JS/data.js
@@ -0,0 +1,72 @@
+const productos = [
+    {
+        id: "product-suculenta",
+        nombre: "Planta Suculenta",
+        descripcion: "Ideal para interiores, requiere poco riego.",
+        precio: 15.00,
+        imagen: "./assets/images/suculenta.jpg"
+    },
+    {
+        id: "product-helecho",
+        nombre: "Helecho",
+        descripcion: "Perfecto para decorar espacios amplios y luminosos.",
+        precio: 25.00,
+        imagen: "./assets/images/helecho.jpg"
+    },
+    {
+        id: "product-cactus",
+        nombre: "Cactus",
+        descripcion: "Fácil de cuidar, recomendado para principiantes.",
+        precio: 10.00,
+        imagen: "./assets/images/cactus.jpg"
+    },
+    {
+        id: "product-bonsai",
+        nombre: "Bonsai",
+        descripcion: "Un toque zen para tu hogar.",
+        precio: 50.00,
+        imagen: "./assets/images/Bonsai.jpg"
+    },
+    {
+        id: "product-aloe-vera",
+        nombre: "Aloe Vera",
+        descripcion: "Ideal para la piel, fácil de cuidar.",
+        precio: 20.00,
+        imagen: "./assets/images/aloe-vera.jpg"
+    },
+    {
+        id: "product-lavanda",
+        nombre: "Lavanda",
+        descripcion: "Planta aromática para interiores y exteriores.",
+        precio: 18.00,
+        imagen: "./assets/images/lavanda-officinalis.jpg"
+    },
+    {
+        id: "product-orquidea",
+        nombre: "Orquídea",
+        descripcion: "Flor delicada y elegante.",
+        precio: 35.00,
+        imagen: "./assets/images/orquidea.webp"
+    },
+    {
+        id: "product-palmera",
+        nombre: "Palmera de interior",
+        descripcion: "Añade frescura y estilo a tu hogar.",
+        precio: 45.00,
+        imagen: "./assets/images/palmera.jpg"
+    },
+    {
+        id: "product-hiedra",
+        nombre: "Hiedra",
+        descripcion: "Planta trepadora perfecta para exteriores.",
+        precio: 22.00,
+        imagen: "./assets/images/hiedra.jpg"
+    },
+    {
+        id: "product-ficus",
+        nombre: "Ficus",
+        descripcion: "Un clásico para interiores luminosos.",
+        precio: 30.00,
+        imagen: "./assets/images/ficus.jpg"
+    }
+];
